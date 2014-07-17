71508050bf18a3f97d0a9d6f9d78ae13a79c06aa
alvin <share.code@aol.com>
2014-07-16 21:23:25 -0500

    adding shebang to the script

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom alvinhenrick/hadoop-dn
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
 docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom alvinhenrick/hadoop-nn-dn
